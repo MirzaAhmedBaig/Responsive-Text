a67809e2ede6e5137e2c25b9272ffaad902b88ad
Mirza Ahmed Baig <mirzaahmed65@gmail.com>
2017-02-09 00:44:44 +0530

    Created responsive tet library also added Demo page for displaying various text headings.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!--Created by MIRZA AHMED BAIG-->
+<!--Date: 08-02-2017-->
+<!--Time: 10:06 PM-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/responsive-text.min.css">
+    <title>Responsive Text Library</title>
+</head>
+<body>
+<div class="resp-container resp-text">
+    <h1>This is heading 1</h1>
+    <h2>This is heading 2</h2>
+    <h3>This is heading 3</h3>
+    <h4>This is heading 4</h4>
+    <h5>This is heading 5</h5>
+    <h6>This is heading 6</h6>
+    <p>Hi this is responsive text library. This library is used to adjust font size of text according to ur screen
+        Hi this is responsive text library. This library is used to adjust font size of text according to ur screen
+        Hi this is responsive text library. This library is used to adjust font size of text according to ur screen
+        Hi this is responsive text library. This library is used to adjust font size of text according to ur screen
+        Hi this is responsive text library. This library is used to adjust font size of text according to ur screen
+        Hi this is responsive text library. This library is used to adjust font size of text according to ur screen</p>
+</div>
+
+</body>
+</html>
